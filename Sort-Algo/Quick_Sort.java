43837dcc6da2d7d033889eaec72ebc33ad7edab8
VINAYAK MISHRA <vmaugust8@gmail.com>
2025-07-18 21:13:06 +0530

    Update Quick_Sort.java

--- Sort-Algo/Quick_Sort.java
+++ Sort-Algo/Quick_Sort.java
@@ -1,8 +1,19 @@
-public class QuickSort {
+import java.util.Scanner;
 
+public class Quick_Sort 
+{
+     public static void BeforeSort(int arr[])
+    {
+      for(int num:arr)
+      {
+        System.out.print(num + " ");
+      }
+    }
     // Function to perform Quick Sort
-    public static void quickSort(int[] arr, int low, int high) {
-        if (low < high) {
+    public static void quickSort(int[] arr, int low, int high) 
+    {
+        if (low < high) 
+        {
             // Partition the array
             int pi = partition(arr, low, high);
 
@@ -11,9 +22,9 @@ public class QuickSort {
             quickSort(arr, pi + 1, high);
         }
     }
-
     // Function to partition the array
-    public static int partition(int[] arr, int low, int high) {
+    public static int partition(int[] arr, int low, int high) 
+    {
         int pivot = arr[high];  // pivot
         int i = low - 1;        // Index of smaller element
 
@@ -23,36 +34,45 @@ public class QuickSort {
                 i++;
 
                 // swap arr[i] and arr[j]
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+                swap(arr,i,j);
             }
         }
-
         // swap arr[i+1] and arr[high] (or pivot)
-        int temp = arr[i + 1];
-        arr[i + 1] = arr[high];
-        arr[high] = temp;
-
+        swap(arr, i + 1,high);
         return i + 1;
     }
-
-    // Utility method to print the array
-    public static void printArray(int[] arr) {
-        for (int num : arr)
-            System.out.print(num + " ");
-        System.out.println();
+    
+    public static void swap(int arr[],int i,int j)
+    {
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
     }
-
+    public static void AfterSort(int arr[])
+    {
+      for(int num:arr)
+      {
+        System.out.print(num + " ");
+      }
+    }
     // Main method to run and test the sort
-    public static void main(String[] args) {
-        int[] arr = {10, 7, 8, 9, 1, 5};
-        System.out.println("Original array:");
-        printArray(arr);
+    public static void main(String[] args) 
+    {
+      Scanner sc=new Scanner(System.in);
+      System.err.println("Enter Size of array");
+      int n=sc.nextInt();
+      int[] arr = new int[n];
+      System.out.println("Enter elements of array");
+      for (int i = 0; i < n; i++)
+      {
+         arr[i] = sc.nextInt();
+      }
+      System.out.println("Array before sorting");
+      BeforeSort(arr);
+      System.out.println();
+      quickSort(arr, 0, n - 1);
+      System.out.println("Array after sorting");
+      AfterSort(arr);
 
-        quickSort(arr, 0, arr.length - 1);
-
-        System.out.println("Sorted array:");
-        printArray(arr);
     }
 }
