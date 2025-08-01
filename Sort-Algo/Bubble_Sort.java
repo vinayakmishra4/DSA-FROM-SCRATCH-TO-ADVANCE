00c5923c227ac78516282c1677774c5d3e594de0
VINAYAK MISHRA <vmaugust8@gmail.com>
2025-08-01 19:54:36 +0530

    Update Bubble_Sort.java

--- Sort-Algo/Bubble_Sort.java
+++ Sort-Algo/Bubble_Sort.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Bubble_Sort
 {
+  // Method to display the array before sorting
   public static void BeforeSort(int arr[])
   {
      for(int num:arr)
@@ -10,22 +11,29 @@ public class Bubble_Sort
      }
   }
 
+  // Method to perform Bubble Sort on the array
   public static void Bubble(int arr[])
   {
-     int n=arr.length;
+     int n = arr.length;
      boolean swaped;
-     for(int i=0;i<n-1;i++)
+     
+     // Outer loop to traverse through all elements
+     for(int i = 0; i < n - 1; i++)
      {
-        swaped=false;
-        for(int j=0;j<n-i-1;j++)
+        swaped = false;
+        
+        // Inner loop for pairwise comparison and swapping
+        for(int j = 0; j < n - i - 1; j++)
         {
-            if(arr[j]>arr[j+1])
+            // Swap if the current element is greater than the next
+            if(arr[j] > arr[j + 1])
             {
-              swap(arr, j, j+1);
-              swaped=true;
+              swap(arr, j, j + 1);
+              swaped = true;
             }
-           
         }
+        
+        // If no elements were swapped in inner loop, the array is sorted
         if(!swaped)
         {
           break;
@@ -33,6 +41,7 @@ public class Bubble_Sort
      }
   }
 
+  // Method to display the array after sorting
   public static void AfterSort(int arr[])
   {
      for(int num:arr)
@@ -40,28 +49,37 @@ public class Bubble_Sort
        System.out.print(num + " ");
      }
   }
-  public static void swap(int arr[],int i,int j)
+
+  // Utility method to swap two elements in the array
+  public static void swap(int arr[], int i, int j)
   {
-    int temp=arr[i];
-    arr[i]=arr[j];
-    arr[j]=temp;
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
   }
-   public static void main(String[] args)
-   {
-     Scanner sc=new Scanner(System.in);
+
+  // Main method to take user input and perform sorting
+  public static void main(String[] args)
+  {
+     Scanner sc = new Scanner(System.in);
+
      System.out.println("Enter the size of array");
-     int n=sc.nextInt();
-     int arr[]=new int[n];
+     int n = sc.nextInt(); // Read array size
+     int arr[] = new int[n]; // Declare array of given size
+
      System.out.println("Enter the number of elements in arr");
-     for(int i=0;i<n;i++)
+     for(int i = 0; i < n; i++)
      {
-       arr[i]=sc.nextInt();
+       arr[i] = sc.nextInt(); // Read array elements
      }
+
      System.out.println("Before sorting");
-     BeforeSort(arr);
+     BeforeSort(arr); // Display original array
      System.out.println();
-     Bubble(arr);
+
+     Bubble(arr); // Sort the array using Bubble Sort
+
      System.out.println("After sorting");
-     AfterSort(arr);
-   } 
+     AfterSort(arr); // Display sorted array
+  } 
 }
