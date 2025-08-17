0d75b9796b8204db61fc7b49a9363f47897b2d8e
VINAYAK MISHRA <vmaugust8@gmail.com>
2025-08-17 16:36:55 +0530

    Create Majortyelement2.java

---
+++ Hard-Array/Majortyelement2.java
@@ -0,0 +1,47 @@
+public class  Majortyelement2
+{
+    public static int majorityElement(int[] nums) {
+        int count = 0, candidate = -1;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+                count = 1;
+            } else if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        // Verify the candidate
+        count = 0;
+        for (int num : nums) {
+            if (num == candidate) {
+                count++;
+            }
+        }
+
+        return (count > nums.length / 3) ? candidate : -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        int result = majorityElement(arr);
+        if (result == -1) {
+            System.out.println("No Majority Element");
+        } else {
+            System.out.println("Majority Element is " + result);
+        }
+    }
+    
+}
