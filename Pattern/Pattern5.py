e287f03f97e112f9ef9d4f0caaf154996ec213db
Blaghul Rizwan <vmaugust8@gmail.com>
2026-01-03 11:17:51 +0530

    Create Pattern5.py

---
+++ Pattern/Pattern5.py
@@ -0,0 +1,14 @@
+# daminod patter printing 
+rows = int(input("Enter the row size for the pattern: "))
+for i in range(1,rows + 1):  # Upper part of diamond
+    for j in range(rows  - i):
+        print(" ", end=" ")
+    for k in range(1, 2 * i):
+        print("*", end=" ")
+    print()
+for i in range(rows  - 1, 0, -1):  # Lower part of diamond
+    for j in range(rows  - i):
+        print(" ", end=" ")
+    for k in range(1, 2 * i):
+        print("*", end=" ")
+    print()
