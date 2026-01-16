937727ea1b4e5815992e8156d984ccd0459478ee
VINAYAK MISHRA <vmaugust8@gmail.com>
2026-01-16 17:57:44 +0530

    Create Pattern12.py

---
+++ Pattern/Pattern12.py
@@ -0,0 +1,11 @@
+# WAP to Number Right-Angled Triangle Pattern in Python 
+
+number=int(input("Enter the number of rows: "))
+
+def number_right_angled_triangle(n):
+    for i in range(1, n + 1):
+        for j in range(1, i + 1):
+            print(j, end=" ")
+        print()
+
+number_right_angled_triangle(number)
