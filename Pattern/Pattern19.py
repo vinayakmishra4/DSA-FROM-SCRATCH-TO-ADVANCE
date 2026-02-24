7042eb1dbf84142a69e21aa8c3fcbc640748ea6b
Blaghul Rizwan <vmaugust8@gmail.com>
2026-02-24 15:13:07 +0530

    up

---
+++ Pattern/Pattern19.py
@@ -0,0 +1,11 @@
+# pattern of pirymand of the alphabets
+
+rows = int(input("Enter the row size for the pattern: "))
+for i in range(1, rows + 1):  # Outer loop for rows
+    for j in range(rows  - i):  # Inner loop for spaces
+        print(" ", end=" ")
+    for k in range(1, i + 1):  # Inner loop for numbers
+        print(chr(64+k), end=" ")
+    for l in range(i - 1, 0, -1):  # Inner loop for reverse numbers
+        print(chr(64+l), end=" ")
+    print()
