e79db73601922a8cfe185e6489ff80d98110806c
vinayakmishra4 <vmaugust8@gmail.com>
2026-03-06 00:18:07 +0530

    Create Pattern27.py

---
+++ Pattern/Pattern27.py
@@ -0,0 +1,11 @@
+# print the hollow square pattern
+
+rows=int(input("Enter the number of rows: "))
+
+for i in range(1,rows+1):
+    for j in range(1, rows+1):
+        if i==1 or i==rows or j==1 or j==rows:
+            print( j , end=" ")
+        else:
+            print(" ", end=" ")
+    print()
