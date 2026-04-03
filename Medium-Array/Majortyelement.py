d65cf8ac1d9c36ffb0db41986f6cf98fbfc5404e
VINAYAK MISHRA <vmaugust8@gmail.com>
2026-04-04 02:40:26 +0530

    Update Majortyelement.py

--- Medium-Array/Majortyelement.py
+++ Medium-Array/Majortyelement.py
@@ -1,18 +1,21 @@
 # WAP to find the majority element in an array. The majority element is the element that appears more than n/2 times in the array.
 
 def majority_element(nums):
-    count = 0
-    candidate = None
-
+    count = {}
     for num in nums:
-        if count == 0:
-            candidate = num
-        count += (1 if num == candidate else -1)
+        if num in count:
+            count[num] += 1
+        else:
+            count[num] = 1
 
-    return candidate
+    for num, freq in count.items():
+        if freq > len(nums) // 2:
+            return num
 
-arr=[]
-n=int(input("Enter the number of elements in the array: "))
+    return None
+
+arr = []
+n = int(input("Enter the number of elements in the array: "))
 print("Enter the elements of the array:")
 for i in range(n):
     element=int(input())
