11d820c0607255471dc9d02168648460c288ac4d
VINAYAK MISHRA <vmaugust8@gmail.com>
2026-04-15 11:11:25 +0530

    Update LeaderArray.py

--- Medium-Array/LeaderArray.py
+++ Medium-Array/LeaderArray.py
@@ -1,17 +1,21 @@
 # WAP Program to print the largest elements (leaders) in the array first
 
 def leader_array(arr):
-    n=len(arr)
-    leaders=[]
-    max_from_right=arr[n-1]
-    leaders.append(max_from_right)
-    for i in range(n-2,-1,-1):
-        if arr[i]>max_from_right:
-            max_from_right=arr[i]
-            leaders.append(max_from_right)
+    n = len(arr)                     # Get the length of the array
+    leaders = []                     # List to store leader elements
 
-    leaders.reverse()    
-    return leaders
+    max_from_right = arr[n-1]        # Start with the last element
+    leaders.append(max_from_right)   # Last element is always a leader
+
+    # Traverse the array from right to left (excluding last element)
+    for i in range(n-2, -1, -1):
+        # If current element is greater than max seen so far from right
+        if arr[i] > max_from_right:
+            max_from_right = arr[i]  # Update max_from_right
+            leaders.append(max_from_right)  # Add it as a leader
+
+    leaders.reverse()                # Reverse to maintain original order
+    return leaders                   # Return the list of leaders
 
 arr=[]
 n=int(input("Enter the number of elements in the array: "))
