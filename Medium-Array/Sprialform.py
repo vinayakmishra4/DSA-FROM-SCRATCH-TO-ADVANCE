6952bdb083761594a9abb7658b35ea9b98fe2804
VINAYAK MISHRA <vmaugust8@gmail.com>
2026-04-20 10:27:50 +0530

    uyp

---
+++ Medium-Array/Sprialform.py
@@ -0,0 +1,43 @@
+# Problem Statement: Given a Matrix, print the given matrix in spiral order.
+
+def spiralOrder(matrix):
+    if not matrix:
+        return []
+    
+    result = []
+    top, bottom = 0, len(matrix) - 1
+    left, right = 0, len(matrix[0]) - 1
+    
+    while top <= bottom and left <= right:
+        # Traverse from left to right
+        for i in range(left, right + 1):
+            result.append(matrix[top][i])
+        top += 1
+        
+        # Traverse from top to bottom
+        for i in range(top, bottom + 1):
+            result.append(matrix[i][right])
+        right -= 1
+        
+        if top <= bottom:
+            # Traverse from right to left
+            for i in range(right, left - 1, -1):
+                result.append(matrix[bottom][i])
+            bottom -= 1
+        
+        if left <= right:
+            # Traverse from bottom to top
+            for i in range(bottom, top - 1, -1):
+                result.append(matrix[i][left])
+            left += 1
+            
+    return result
+
+# Example usage:
+n = int(input())
+m = int(input())
+matrix = []
+for _ in range(n):
+    row = list(map(int, input().split()))
+    matrix.append(row)
+print(spiralOrder(matrix))
