2cd6831b2428e03d75e4af82bde03503e29702bd
VINAYAK MISHRA <vmaugust8@gmail.com>
2026-05-03 04:42:49 +0530

    up

---
+++ Hard-Array/Largestsum0.py
@@ -0,0 +1,7 @@
+# WAP to find the Given an array containing both positive and negative integers, we have to find the length of the longest subarray with the sum of all elements equal to zero.
+
+# brute froce approach
+def belargestsum(arr):
+    # lenght of the array
+    n=len(arr)
+    
