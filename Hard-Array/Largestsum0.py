b82b16a1545970c8a6940042f559890fe2d31e12
VINAYAK MISHRA <vmaugust8@gmail.com>
2026-05-04 07:47:39 +0530

    Update Largestsum0.py

--- Hard-Array/Largestsum0.py
+++ Hard-Array/Largestsum0.py
@@ -26,13 +26,34 @@ def longest_zero_sum_bruteforce(arr):
     # Return the length of the longest subarray with sum = 0
     return max_length
 
+# better apporach
+def longest_zero_sum_better(arr):
+    n = len(arr)          # Total number of elements in array
+    max_length = 0        # Stores the maximum length of subarray with sum = 0
+
+    # Outer loop → selects the starting index of subarray
+    for i in range(n):
+        sub_sum = 0       # Reset sum for each new starting point
+
+        # Inner loop → selects the ending index of subarray
+        for j in range(i, n):
+            sub_sum += arr[j]   # Add current element to subarray sum
+
+            # If sum becomes 0 → valid subarray found
+            if sub_sum == 0:
+                # Update max_length if current subarray is longer
+                max_length = max(max_length, j - i + 1)
+
+    return max_length     # Return the maximum length found
+
+
 nums=int(input("Enter the number of the elements :-"))
 arr=[]
 print("Enter the elements :-")
 for i in range(nums):
     i=int(input())
     arr.append(arr[i])
-result=longest_zero_sum_bruteforce(arr)
+result=longest_zero_sum_better(arr)
 print("Longest length of the Array with sum zero = ",result)
 
 
