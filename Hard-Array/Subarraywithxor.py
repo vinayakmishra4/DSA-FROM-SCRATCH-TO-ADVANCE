49f8234e606b1024b5cf16c4dc928bbfd08b8aad
VINAYAK MISHRA <vmaugust8@gmail.com>
2026-05-08 18:53:34 +0530

    Update Subarraywithxor.py

--- Hard-Array/Subarraywithxor.py
+++ Hard-Array/Subarraywithxor.py
@@ -1,5 +1,5 @@
 # WAP to find Given an array of integers A and an integer B. Find the total number of subarrays having bitwise XOR of all elements equal to k.
-
+from collections import defaultdict
 # brute force approach
 def brsubarraywithxor(a, b):
 
@@ -23,6 +23,32 @@ def brsubarraywithxor(a, b):
                 count += 1
 
     # Return total count of subarrays
+    return count
+
+
+# Optimal Approach
+def opsubarrayWithXor(a, b):
+    freq = defaultdict(int)
+
+    # Prefix XOR 0 occurs once initially
+    freq[0] = 1
+
+    prefix_xor = 0
+    count = 0
+
+    for num in a:
+        # Compute prefix XOR
+        prefix_xor ^= num
+
+        # Required previous XOR
+        required = prefix_xor ^ b
+
+        # Add matching prefixes
+        count += freq[required]
+
+        # Store current prefix XOR
+        freq[prefix_xor] += 1
+
     return count
 
 # Input array elements
