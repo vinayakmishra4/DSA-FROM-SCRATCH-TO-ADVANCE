27eb29e00f6a96d6b877fcb07d722e19d6bdd643
VINAYAK MISHRA <vmaugust8@gmail.com>
2026-06-25 05:06:11 +0530

    Update FristandLast.py

--- Binary-Sreach-1D/FristandLast.py
+++ Binary-Sreach-1D/FristandLast.py
@@ -4,12 +4,29 @@
 # coding 360:-https://www.naukri.com/code360/problems/first-and-last-position-of-an-element-in-sorted-array_1082549
 
 
-def find(arr, x):
-    # strating and ending point for the binary serach
-    low=0
-    high=len(arr)-1
-    list=[]
+def first_occurrence(arr, x):
+    left = 0
+    right = len(arr) - 1
+    ans = -1
 
-    # using while loop to find the frist and last occurnce  
-    while low < high:
-        mid=(low + high)//2 
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] == x:
+            ans = mid          # store possible answer
+            right = mid - 1   # search further left
+
+        elif arr[mid] < x:
+            left = mid + 1
+
+        else:
+            right = mid - 1
+
+    return ans
+
+# Last occurance
+def Lastoccurance(arr,x):
+
+# comibing the frist and last of occurance
+def find(arr, x):
+    #code here
