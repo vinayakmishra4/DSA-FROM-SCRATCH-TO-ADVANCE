24291d3eb7f30544514342feff5329274621964e
VINAYAK MISHRA <vmaugust8@gmail.com>
2026-06-26 02:27:09 +0530

    Update FristandLast.py

--- Binary-Sreach-1D/FristandLast.py
+++ Binary-Sreach-1D/FristandLast.py
@@ -3,7 +3,7 @@
 # geeks for geeks :-https://www.geeksforgeeks.org/problems/first-and-last-occurrences-of-x3116/1
 # coding 360:-https://www.naukri.com/code360/problems/first-and-last-position-of-an-element-in-sorted-array_1082549
 
-
+# First occurrence of element in array
 def first_occurrence(arr, x):
     left = 0
     right = len(arr) - 1
@@ -13,8 +13,8 @@ def first_occurrence(arr, x):
         mid = (left + right) // 2
 
         if arr[mid] == x:
-            ans = mid          # store possible answer
-            right = mid - 1   # search further left
+            ans = mid
+            right = mid - 1  # search further left
 
         elif arr[mid] < x:
             left = mid + 1
@@ -24,9 +24,44 @@ def first_occurrence(arr, x):
 
     return ans
 
-# Last occurance
-def Lastoccurance(arr,x):
 
-# comibing the frist and last of occurance
+# Last occurrence of element in array
+def last_occurrence(arr, x):
+    left = 0
+    right = len(arr) - 1
+    ans = -1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] == x:
+            ans = mid
+            left = mid + 1  # search further right
+
+        elif arr[mid] < x:
+            left = mid + 1
+
+        else:
+            right = mid - 1
+
+    return ans
+
+
+# Combining first and last occurrence
 def find(arr, x):
-    #code here
+    first = first_occurrence(arr, x)
+    last = last_occurrence(arr, x)
+    return [first, last]
+
+
+# User Input
+n = int(input("Enter the number of elements: "))
+
+print("Enter the elements in sorted order:")
+arr = list(map(int, input().split()))
+
+x = int(input("Enter the element to find: "))
+
+result = find(arr, x)
+
+print("First and Last Occurrence:", result)
