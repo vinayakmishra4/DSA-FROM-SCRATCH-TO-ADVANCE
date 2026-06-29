9873294724d1106598d5a3807976bf4b21a1cc57
VINAYAK MISHRA <vmaugust8@gmail.com>
2026-06-29 07:45:45 +0530

    up

---
+++ Binary-Sreach-1D/Sreachpostion.py
@@ -0,0 +1,22 @@
+# WAP to find the sreach insertion postion in the array
+# leetcode :-https://leetcode.com/problems/search-insert-position/
+# geeks for geeks:https://www.geeksforgeeks.org/problems/search-insert-position-of-k-in-a-sorted-array/1
+# code360:-https://www.naukri.com/code360/problems/algorithm-to-find-best-insert-position-in-sorted-array_839813?leftPanelTabValue=SUBMISSION
+
+# find or inset in the array 
+class Solution:
+    def searchInsertK(self, arr, k):
+        pass
+        
+
+
+
+
+
+
+arr=[]
+num=int(input(" Enter the size of array :- "))
+print(" Enter the elements ")
+for i in range(num):
+    i=int(input())
+    arr.append(i)
