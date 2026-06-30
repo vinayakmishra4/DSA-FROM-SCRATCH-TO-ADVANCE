78df4de87472a0253dbb7e9171dc24c3dc0c0144
VINAYAK MISHRA <vmaugust8@gmail.com>
2026-06-30 15:34:54 +0530

    Update Sreachpostion.py

--- Binary-Sreach-1D/Sreachpostion.py
+++ Binary-Sreach-1D/Sreachpostion.py
@@ -4,10 +4,10 @@
 # code360:-https://www.naukri.com/code360/problems/algorithm-to-find-best-insert-position-in-sorted-array_839813?leftPanelTabValue=SUBMISSION
 
 # find or inset in the array 
-class Solution:
-    def searchInsertK(self, arr, k):
-        pass
-        
+def searchInsertK(arr, k):
+    for i in range(len(arr)):
+        if arr[i]==k :
+            return i        
 
 
 
@@ -20,3 +20,6 @@ print(" Enter the elements ")
 for i in range(num):
     i=int(input())
     arr.append(i)
+k=int(input("Enter the number to insert sreach postion"))
+result=searchInsertK(arr, k)
+print("Position of the number : " , searchInsertK(arr, k))
