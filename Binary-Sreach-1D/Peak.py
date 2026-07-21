9c3c66a6f54f62ba4b9ee0785d2ff02b42e2db0c
VINAYAK MISHRA <vmaugust8@gmail.com>
2026-07-22 03:47:43 +0530

    Update Peak.py

--- Binary-Sreach-1D/Peak.py
+++ Binary-Sreach-1D/Peak.py
@@ -3,3 +3,20 @@
 def peakelement(arr):
     low=0
     high=len(arr)-1
+
+    while low<high:
+        mid=low+(low+high)//2
+        if arr[mid]<arr[mid+1]:
+            low=mid+1
+        else:
+            high=mid
+    return arr[low]
+
+nums=int(input("Enter the number of elements in the array: "))
+arr=[]
+for i in range(nums):
+    element=int(input("Enter the element: "))
+    arr.append(element)
+result=peakelement(arr)
+print("The peak element in the array is:", result)
+
